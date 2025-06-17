b0b0e5d2874bdd10e79faea1a82bce34f3bd89d1
Aneesha-raj <aneesharaj.k13@gmail.com>
2025-06-17 14:26:09 +0530

    Create CircleArea.java

---
+++ CircleArea.java
@@ -0,0 +1,7 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        double r = 7.0;
+        double area = 3.14159 * r * r;
+        System.out.println("Area: " + area);
+    }
+}
